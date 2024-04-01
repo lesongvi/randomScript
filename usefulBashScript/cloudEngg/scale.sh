7682b80639758eb407d38f7675e2c6820e7c0050
Lê Song Vĩ <lesongvi@gmail.com>
2024-04-01 12:59:55 +0700

    Update scale.sh

--- usefulBashScript/cloudEngg/scale.sh
+++ usefulBashScript/cloudEngg/scale.sh
@@ -26,11 +26,12 @@ fi
 
 if [ "$userAnswer" == "no" ]; then
   echo "User chose to not continue scaling ;)"
-  exit
+  exit 0
 else
   echo "I hope you know what you are doing"
 fi
 
+exit 0
 
 echo eksctl scale nodegroup --cluster $CLUSTERNAME --name $CLUSTERNAME-eks-nodes --nodes $NUMBEROFNODE --nodes-min $NUMBEROFNODE --nodes-max $NUMBEROFNODE
 
