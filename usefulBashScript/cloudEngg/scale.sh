ab0ae7241174b013127def0efd7a7d68d3520d7e
Lê Song Vĩ <lesongvi@gmail.com>
2024-04-01 12:45:59 +0700

    Money talk ;)

--- usefulBashScript/cloudEngg/scale.sh
+++ usefulBashScript/cloudEngg/scale.sh
@@ -7,12 +7,30 @@ fi
 
 declare -A CLUSTER_MAP
 CLUSTER_MAP=(
-    [v]="vicluster"
-    [v1]="vicluster1"
+    [g]="genaihvnapp"
+    [g1]="genaihvnapp1"
 )
 
 CLUSTERNAME="${CLUSTER_MAP[$1]:-$1}"
 NUMBEROFNODE="${2:-1}"
+
+userAnswer=""
+
+if (( NUMBEROFNODE > 1 )); then
+  while [[ "$userAnswer" != "yes" && "$userAnswer" != "no" ]];
+  do
+    echo "You are attempting to scale 3 nodes for the cluster $CLUSTERNAME, are you sure? (yes/no)"
+    read userAnswer
+  done
+fi
+
+if [ "$userAnswer" == "no" ]; then
+  echo "User chose to not continue scaling ;)"
+  exit
+else
+  echo "I hope you know what you are doing"
+fi
+
 
 echo eksctl scale nodegroup --cluster $CLUSTERNAME --name $CLUSTERNAME-eks-nodes --nodes $NUMBEROFNODE --nodes-min $NUMBEROFNODE --nodes-max $NUMBEROFNODE
 
