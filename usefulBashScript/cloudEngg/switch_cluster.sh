b8b85144e7461763251c3019e65662f4fbec0065
Lê Song Vĩ <lesongvi@gmail.com>
2024-04-01 12:59:35 +0700

    Update switch_cluster.sh

--- usefulBashScript/cloudEngg/switch_cluster.sh
+++ usefulBashScript/cloudEngg/switch_cluster.sh
@@ -16,3 +16,5 @@ CLUSTERNAME="${CLUSTER_MAP[$1]:-$1}"
 echo aws eks --region ap-northeast-1 update-kubeconfig --name $CLUSTERNAME
 
 aws eks --region ap-northeast-1 update-kubeconfig --name $CLUSTERNAME
+
+exit 0
