5eccb019b64b645bcd59acdd020f0919cbe41db7
Lê Song Vĩ <lesongvi@gmail.com>
2024-04-01 13:29:41 +0700

    Create getpods.sh

---
+++ usefulBashScript/cloudEngg/getpods.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <namespace>"
+  exit 1
+fi
+
+declare -A NAMESPACE_MAP
+NAMESPACE_MAP=(
+    [vs]="vcluster-service"
+    [va]="vcluster-auth"
+    [v1s]="vcluster1-service"
+    [v1a]="vcluster1-auth"
+)
+
+NAMESPACE="${NAMESPACE_MAP[$1]:-$1}"
+
+echo kubectl -n "$NAMESPACE" get pods
+
+kubectl -n "$NAMESPACE" get pods
+
+exit 0
